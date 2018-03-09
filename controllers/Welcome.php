8ccf6b65bbea5479623399f667e9489876a0cb6b
gustavo villas boas <gu.boas13@gmail.com>
2018-03-09 12:42:19 -0300

    Configura o eloquent

--- controllers/Welcome.php
+++ controllers/Welcome.php
@@ -18,8 +18,10 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
+	public function index() {
+
+		$users = User::all(); // will fetch all users
+		var_dump( $users->first()->username );
 		$this->load->view('welcome_message');
 	}
 }
